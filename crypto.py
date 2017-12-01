30e78b8d5b9861ddec5c2dccd4a25d5e35e2eadb
Zachary Weeden <zweeden@datto.com>
2017-11-30 20:35:05 -0500

    One liner now

--- crypto.py
+++ crypto.py
@@ -4,8 +4,6 @@ headers = {'CB-VERSION':datetime.datetime.now().strftime("%Y-%m-%d")}
 session = requests.session()
 while 1:
 	btc_r = session.get('https://api.coinbase.com/v2/prices/BTC-USD/buy', headers=headers)
-	print(f'{datetime.datetime.now()} :: {btc_r.json()["data"]}')
 	eth_r = session.get('https://api.coinbase.com/v2/prices/ETH-USD/buy', headers=headers)
-	print(f'{datetime.datetime.now()} :: {eth_r.json()["data"]}')
-	print()
+	print(f'{datetime.datetime.now()} :: {btc_r.json()["data"]["base"]}: {btc_r.json()["data"]["amount"]} && {eth_r.json()["data"]["base"]}: {eth_r.json()["data"]["amount"]}')
 	time.sleep(15)
